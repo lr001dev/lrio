1dc873d4832e9904414d4802edb81b92da30d2cc
Luis Rosario <luisrosariomix@gmail.com>
2019-09-02 11:10:32 -0700

    Fixed about image. Adjusted css for mobile. Tweaking projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,9 +21,8 @@ class Projects extends Component {
           <Card.Title>Chat Party</Card.Title>
           <Card.Img variant="top" src="/img/chat_party.png" />
           <Card.Body>
-
             <Card.Text>
-              Chat Party is live chat MVC/CRUD application that allows users to create their own chat rooms and host group chats. I was interested in creating an application that used some kind of socket framework. I have been reading about Socket IO and it's js library to understand how sockets work and if I could successfully implement it and send events from the front end to the back end. This was a fun journey that hit the project requirements and achieved all the stretch goals.
+              Chat Party is live chat MVC/CRUD application that allows users to create their own chat rooms and host group chats. I was interested in creating an application that used some kind of socket framework. I have been reading about Socket IO and it's js library to understand how sockets work.  n This was a fun journey that hit the project requirements and achieved all the stretch goals.
             </Card.Text>
           </Card.Body>
           <ListGroup variant="flush">
@@ -31,8 +30,8 @@ class Projects extends Component {
             <ListGroup.Item>EJS | Javascript | HTML5 | Bootstrap | Deployment Heroku </ListGroup.Item>
           </ListGroup>
           <Card.Footer>
-          <Button className="buttons" variant="outline-success" size="lg"><a href="https://chat-party.herokuapp.com">Live Demo</a></Button>
-          <Button className="buttons" variant="outline-success" size="lg"><a href="https://github.com/lr001dev/chatParty">Check Out The Code</a></Button>
+          <Button className="buttons" variant="outline-success" size="lg"><a href="https://chat-party.herokuapp.com" target="_blank">Live Demo</a></Button>
+          <Button className="buttons" variant="outline-success" size="lg"><a href="https://github.com/lr001dev/chatParty" target="_blank" >Check Out The Code</a></Button>
           </Card.Footer>
         </Card>
         </div>
@@ -52,8 +51,8 @@ class Projects extends Component {
             <ListGroup.Item>React | Javascript | Bootstrap | Deployment Heroku </ListGroup.Item>
           </ListGroup>
           <Card.Footer>
-          <Button className="buttons" variant="outline-success" size="lg"><a href="https://react-fitness.herokuapp.com">Live Demo</a></Button>
-          <Button className="buttons" variant="outline-success" size="lg"><a href="https://github.com/lr001dev/reactStripeDashboardClient">Check Out The Front End Code</a></Button>
+          <Button className="buttons" variant="outline-success" size="lg"><a href="https://react-fitness.herokuapp.com" target="_blank">Live Demo</a></Button>
+          <Button className="buttons" variant="outline-success" size="lg"><a href="https://github.com/lr001dev/reactStripeDashboardClient" target="_blank">Check Out The Front End Code</a></Button>
           </Card.Footer>
         </Card>
         </div>
@@ -65,7 +64,7 @@ class Projects extends Component {
       <Card.Body>
 
         <Card.Text>
-          Food And Fortune is a random restaurant chooser application based on specialized food collections specific to a city. The app is based on the Zomato Restaurant and Fortune Cookie API. Users load a city and receive random selections of restaurants to guide them to their next food destination. The app also displays 1 Fortune from the Fortune Cookie API with each search.
+          Food And Fortune is a random restaurant chooser front end application based on specialized food collections specific to a city. The app is based on the Zomato Restaurant and Fortune Cookie API. Users load a city and receive random selections of restaurants to guide them to their next food destination. The app also displays 1 Fortune from the Fortune Cookie API with each search.
         </Card.Text>
       </Card.Body>
       <ListGroup variant="flush">
@@ -73,8 +72,8 @@ class Projects extends Component {
         <ListGroup.Item>Zomato API | Fortune Cookie API | HTML5 Audio | Git Hub Pages </ListGroup.Item>
       </ListGroup>
       <Card.Footer>
-      <Button className="buttons" variant="outline-success" size="lg"><a href="https://chat-party.herokuapp.com">Live Demo</a></Button>
-      <Button className="buttons" variant="outline-success" size="lg"><a href="https://lr001dev.github.io/Food-And-Fortune-WebApp/">Check Out The Code</a></Button>
+      <Button className="buttons" variant="outline-success" size="lg"><a href="https://chat-party.herokuapp.com" target="_blank">Live Demo</a></Button>
+      <Button className="buttons" variant="outline-success" size="lg"><a href="https://lr001dev.github.io/Food-And-Fortune-WebApp/" target="_blank">Check Out The Code</a></Button>
       </Card.Footer>
     </Card>
         </div>
